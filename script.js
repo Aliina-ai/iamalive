1c321b16da4ac44d952b6a30113491873defbd54
Aliina-ai <alinanikipelova1994@gmail.com>
2025-11-29 20:09:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ function draw() {
 }
 
 setInterval(draw, 33);
+
+// SCROLL REVEAL ANIMATION
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+},{ threshold: 0.15 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
